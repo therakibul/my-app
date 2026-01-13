1e1cea570526446e46c60451a7d700515083a0e6
therakibul <mrhassan857@gmail.com>
2026-01-13 20:18:02 +0600

    Laravel Route & View

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Hello, Welcome to Contact</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
+Route::view('/', 'home');
+Route::view('about', 'about');
+Route::view('contact', 'contact');
