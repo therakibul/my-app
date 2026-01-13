7cef23cb3ae24e90af38967978f6168fa09d9429
therakibul <mrhassan857@gmail.com>
2026-01-13 20:34:01 +0600

    Laravel Component

---
+++ resources/views/components/alert-box.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <p class="alert-success">{{$message}}</p>
+</div>
+<style>
+    .alert-success{
+        background: green;
+        text-align: center;
+        width: 750px;
+        margin: 0 auto;
+        padding: 30px 0;
+        border-radius: 10px;
+    }
+</style>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,4 @@
 <div>
     <h1>Hello, Welcome to Contact</h1>
+    <x-alert-box message="You are allow to login this site..."/>
 </div>
